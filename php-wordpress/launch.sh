a812d969282ea05a7c15ffcbd0906384ce7a5ece
ccommiss <ccommiss@student.42.fr>
2022-02-28 17:04:29 +0100

    added des trucs mas WP pas fonctionnel

--- php-wordpress/launch.sh
+++ php-wordpress/launch.sh
@@ -1,1 +1,7 @@
-tail -f /dev/null
+wp core download
+wp core config --dbname=mydbname --dbuser=mydbuser --dbpass=mydbpass --dbhost=localhost --dbprefix=whebfubwef_ --extra-php <<PHP
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+PHP
+wp db create
+wp core install --url=http://siteurl.com --title=SiteTitle --admin_user=username --admin_password=mypassword --admin_email=my@email.com
