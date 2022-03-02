22b890c5c87a665ca50d9be622873c2d96032e25
ccommiss <ccommiss@student.42.fr>
2022-03-02 17:44:32 +0100

    all

--- php-wordpress/launch.sh
+++ php-wordpress/launch.sh
@@ -9,8 +9,7 @@ done
 echo "SQL ADMIN REACHED"
 #db create va se servir des infos de wp-config pour faore ;les tables etc
 #wp db create --allow-root
-wp core install --url="localhost" --title="ta mere" --admin_user="wordpress" --admin_password="wordpress" --admin_email="lol@lol.fr" --skip-email --allow-root
-#wp core install --url=http://siteurl.com --title=SiteTitle --admin_user=username --admin_password=mypassword --admin_email=my@email.com
+wp core install --url="ccommiss.42.fr" --title="ta mere" --admin_user="wordpress" --admin_password="wordpress" --admin_email="lol@lol.fr" --skip-email --allow-root
 
 php-fpm7.3 -F -R
 root
